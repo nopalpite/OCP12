e2f7bc692bf81f6aad59557554650026e921286b
nopalpite <nopalpite@gmail.com>
2023-05-21 19:18:21 +0200

    add status model

---
+++ EpicEvents/status/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Status
+
+@admin.register(Status)
+class ClientAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (
+            "Status",
+            {
+                "fields": (
+                    "name",
+                )
+            },
+        ),
+    )
+    list_display = (
+        "name",
+    )
+    list_filter = ("name",)
+    search_fields = ("name",)
